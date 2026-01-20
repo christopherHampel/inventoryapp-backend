811603659e0ebca23c72a992d8b6c4bb84a4fb92
christopherHampel <161190884+christopherHampel@users.noreply.github.com>
2026-01-20 19:52:05 +0100

    enable Cors origin on localhost:4200

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.enableCors({
+    origin: 'http://localhost:4200', // Angular-App
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true, // falls Cookies/Session benötigt werden
+  });
+
+  await app.listen(3000);
+  // await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
